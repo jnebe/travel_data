4e4cdd17fbe015b4768180c298951cbc2fac6cf6
Leo Wilhelm Lierse <lierseleow@gmail.com>
2025-06-09 18:05:07 +0200

    added ability to set --iterations for train.py

--- train.py
+++ train.py
@@ -13,9 +13,10 @@ from gravity_model.gravity import GravityModel, PowerGravityModel, DoublePowerGr
 @click.argument("location_data", metavar="[Location Data]", type=click.Path(exists=True, readable=True, dir_okay=False, path_type=Path))
 @click.argument("model_output", metavar="[Model Output]", type=click.Path(readable=True, dir_okay=False, path_type=Path))
 @click.argument("model_type", metavar="[Model Type]", type=click.Choice(ModelType, case_sensitive=False))
+@click.option("-i", "--iterations", type=int, default=100)
 @click.option("--optimize", type=click.Path(exists=True, readable=True, dir_okay=False, path_type=Path))
 @click.option("-d", "--default-parameter", type=(str, float), multiple=True)
-def main(location_data: Path, model_output: Path, model_type: ModelType, optimize: Path, default_parameter: list[tuple[str, float]]):
+def main(location_data: Path, model_output: Path, model_type: ModelType, optimize: Path, iterations: int,  default_parameter: list[tuple[str, float]]):
     logger.info(f"Loading location data from {location_data.absolute().as_posix()}")
     locs = LocationContainer.from_csv(location_data)
     default_parameter = dict(default_parameter)
@@ -36,9 +37,9 @@ def main(location_data: Path, model_output: Path, model_type: ModelType, optimiz
         if model_type is ModelType.BASIC:
             model.train(target_trips, 1)
         if model_type is ModelType.POWER:
-            model.train(desired=target_trips, iterations=200, accuracy=0.01, parameters={"alpha": (0.1, 5.0, 1.0)})
+            model.train(desired=target_trips, iterations=iterations, accuracy=0.01, parameters={"alpha": (0.1, 5.0, 1.0)})
         if model_type is ModelType.DOUBLEPOWER:
-            model.train(desired=target_trips, iterations=100, accuracy=0.01, parameters={"alpha": (0.1, 0.7, 0.2), "beta": (0.1, 0.7, 0.2)})
+            model.train(desired=target_trips, iterations=iterations, accuracy=0.01, parameters={"alpha": (0.1, 0.7, 0.2), "beta": (0.1, 0.7, 0.2)})
     
     logger.info(f"Storing model at {model_output.absolute().as_posix()}")
     model.to_json(model_output)
