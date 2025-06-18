573bc4297f31c20b541579b2e55b8a19105d5fed
Leo Wilhelm Lierse <lierseleow@gmail.com>
2025-06-18 15:18:20 +0200

    enable flag to maintain distances in convert.py

--- convert.py
+++ convert.py
@@ -12,7 +12,7 @@ from gravity_model.distance import LATypes, BallTreeLocationAssigner, BeeLineLoc
 @click.argument("raw_data", metavar="[Unprocessed Trip Data]", type=click.Path(exists=True, readable=True, dir_okay=False, path_type=Path))
 @click.argument("loc_assigner_type", metavar="[Location Assigner Type]", type=click.Choice(LATypes, case_sensitive=False))
 @click.argument("results_output", metavar="[Trip Data Output]", type=click.Path(readable=True, dir_okay=False, path_type=Path))
-@click.option("--k", "--keep-distance", "keep_distance", is_flag=True)
+@click.option("-k", "--keep-distance", "keep_distance", is_flag=True)
 def main(location_data: Path, raw_data: Path, loc_assigner_type:LATypes, results_output: Path, keep_distance: bool):
     logger.info(f"Loading location data from {location_data.absolute().as_posix()}")
     locs = LocationContainer.from_csv(location_data)
@@ -27,7 +27,7 @@ def main(location_data: Path, raw_data: Path, loc_assigner_type:LATypes, results
     trips = TripLoader.load_trips(
         loc_assigner, raw_data,
         {"start_lat": "home_coord_x", "start_long": "home_coord_y", "stop_lat": "dest_coord_x", "stop_long": "dest_coord_y", "number": "frequency"},
-        keep_distance=True
+        keep_distance=keep_distance
     )
 
     logger.info(f"Saving normalized trip data to {results_output.absolute().as_posix()}")
